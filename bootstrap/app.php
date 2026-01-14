134f4bacde2c7e14988c91786f7d67782fa57f47
qasassmoh <qasass.moh@gamil.com>
2026-01-14 16:35:04 +0300

    change comment at app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Register admin middleware alias
+       
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
